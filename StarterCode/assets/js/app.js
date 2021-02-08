4f580e7784de0f029b71ad2a4b73eb89038fbfcd
Karen Warburton <tidepooler@icloud.com>
2021-02-08 10:52:54 -0800

    setting up charts

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,23 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = parseInt(d3.select('#scatter').style('width')) * 3;
+var svgHeight = svgWidth - svgWidth/2;
+var margin = {top:10, right:10, bottom:10, left:20};
+var width = svgWidth * .75;
+var height = svgHeight * .75;
+
+
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr('class', 'chart')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.select(".chart")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+
